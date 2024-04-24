5a438b14a46336131a8521dff17b91cb1db15d4f
davidrondina <rondinadavid.r@gmail.com>
2024-04-24 23:17:19 +0800

    display student search in the header for faculty user

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -1,7 +1,12 @@
 <header class="w-full justify-end items-center bg-white py-1 px-6 hidden shadow sm:flex border-b border-gray-300">
-    <x-container class="py-3">
+    <x-container class="py-3 grid grid-cols-2 gap-x-5">
+        <div>
+            @if (Auth::user()->hasRole('faculty'))
+                <x-form.search-form :form_action="__('#')" :placeholder="__('Search student name')" />
+            @endif
+        </div>
         <div class="flex justify-end items-center">
-            <form action="{{ route('logout') }}" method="post">
+            <form action="{{ route('logout') }}" method="post" class="flex justify-end">
                 @csrf
                 <button class="inline-flex items-center gap-x-2 btn btn-sm font-fs font-semibold uppercase">
                     <i class="fa-solid fa-right-from-bracket"></i>
