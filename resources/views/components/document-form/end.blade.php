be5a4babcf208284d41c6c5316c4450a5d9d770b
davidrondina <rondinadavid.r@gmail.com>
2024-05-01 15:11:54 +0800

    style document form components

---
+++ resources/views/components/document-form/end.blade.php
@@ -0,0 +1,6 @@
+<div class="mt-28 flex items-center gap-x-2">
+    <span class="flex-1 block border-t border-dashed border-black"></span>
+    <span class="inline-flex items-center gap-x-4 text-sm"><i class="fa-solid fa-scissors"></i>End of document<i
+            class="fa-solid fa-scissors"></i></span>
+    <span class="flex-1 block border-t border-dashed border-black"></span>
+</div>
---
+++ resources/views/components/document-form/header.blade.php
@@ -0,0 +1,25 @@
+<header class="pb-5 flex gap-x-2 min-h-48 border-b-4 border-black">
+    <div class="flex flex-1 items-center justify-center gap-x-3">
+        <div class="w-24 aspect-square"><img class="object-cover" src="{{ asset('images/deped-logo.png') }}" alt="">
+        </div>
+        <div class="w-24 aspect-square"><img class="object-cover" src="{{ asset('images/deped-calabarzon-logo.png') }}"
+                alt=""></div>
+    </div>
+    <div class="flex-2 text-sm pt-2">
+        <h5 class="text-center">
+            Republic of the Philippines<br>
+            Department of Education<br>
+            <span class="text-center font-bold">Region IV-A (CALABARZON)</span><br>
+            CITY SCHOOLS DIVISION OF BACOOR<br>
+            <span class="text-center font-bold text-lg">SHS IN SAN NICOLAS III, BACOOR CITY</span><br>
+            San Nicolas III, Bacoor City, Cavite<br>
+            Tel No. (046) 236 3371 / Cell No. 09-17-1264983
+        </h5>
+    </div>
+    <div class="flex flex-1 items-center justify-center gap-x-3">
+        <div class="w-24 aspect-square"><img class="object-cover"
+                src="{{ asset('images/city-schools-div-bacoor.png') }}" alt=""></div>
+        <div class="w-24 aspect-square"><img class="object-cover" src="{{ asset('images/shssn-logo.png') }}"
+                alt=""></div>
+    </div>
+</header>
