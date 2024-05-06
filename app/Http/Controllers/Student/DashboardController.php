96ec092a0727938dd029bf53fc721fa6deb1f992
davidrondina <rondinadavid.r@gmail.com>
2024-05-06 20:58:18 +0800

    render actual data in student dashboard page

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers\Student;
 
-use App\Http\Controllers\Controller;
+use App\Models\AcademicYear;
+use App\Models\DocumentAcquisition;
+use Carbon\Carbon;
+use App\Models\Section;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('users.student.dashboard');
+        $user = Auth::user();
+
+        $current_year = AcademicYear::where('is_current', true)->first();
+
+        $acquisitions = $user->acquisitions;
+
+        $upcoming_apps = Appointment::whereHas(
+            'complaint',
+            function ($query) use ($user) {
+                $query->where('student_id', $user->studentInfo->id);
+            }
+        )->whereDate('start_date', '>=', Carbon::now())->get();
+
+        $offenses = $user->studentInfo->complaintsReceived();
+
+        $current_section = Section::whereHas('students', function ($query) use ($user) {
+            $query->where('student_id', $user->studentInfo->id);
+        })->where('academic_year_id', $current_year->id)->latest()->first();
+
+        // dd($acquisitions->count(), $offenses->count(), $current_section, $upcoming_apps);
+
+        return view('users.student.dashboard', compact(['acquisitions', 'current_section', 'current_year', 'offenses', 'upcoming_apps', 'user']));
     }
 }
--- resources/views/users/student/dashboard.blade.php
+++ resources/views/users/student/dashboard.blade.php
@@ -1,3 +1,7 @@
+@php
+    use Carbon\Carbon;
+@endphp
+
 <x-app.student.main-container>
     <main class="w-full flex-grow">
         <x-app.page-header :show_title="true" :title="__('Hello, ') . Auth::user()->profile->first_name" />
@@ -18,7 +22,7 @@
                                 <h2 class="font-semibold uppercase text-sm text-gray-500">Total Offenses</h2>
 
                                 <div>
-                                    <span class="text-2xl truncate font-semibold">0</span>
+                                    <span class="text-2xl truncate font-semibold">{{ $offenses->count() }}</span>
                                 </div>
                             </div>
                         </x-card>
@@ -34,7 +38,7 @@
                                 <h2 class="font-semibold uppercase text-sm text-gray-500">Total Acquisitions</h2>
 
                                 <div>
-                                    <span class="text-2xl truncate font-semibold">0</span>
+                                    <span class="text-2xl truncate font-semibold">{{ $acquisitions->count() }}</span>
                                 </div>
                             </div>
                         </x-card>
@@ -42,63 +46,78 @@
                 </div>
 
                 <div class="flex flex-col gap-y-2">
-                    <h2 class="z-10 bg-base-100 md:sticky md:top-0 md:left-0 font-bold text-lg">A.Y. 2024 - 2025</h2>
+                    <h2 class="z-10 bg-base-100 md:sticky md:top-0 md:left-0 font-bold text-lg">A.Y.
+                        {{ $current_year->getFullYear() }}</h2>
                     <x-card class="py-3 px-4 flex flex-col gap-y-8">
-                        <div class="flex flex-col gap-y-4">
-                            <h3 class="font-bold">Your Section: GAS 11-A</h3>
-                            <div class="grid grid-cols-1 md:grid-cols-2 gap-3 font-fs">
-                                <x-card class="py-3 px-4 flex flex-row gap-x-4">
-                                    <div>
-                                        <div
-                                            class="w-12 aspect-square border border-accent rounded-full inline-flex justify-center items-center text-xl text-accent">
-                                            <i class="fa-solid fa-user-graduate"></i>
-                                        </div>
-                                    </div>
-                                    <div class="flex flex-col">
-                                        <h2 class="font-semibold uppercase text-sm text-gray-500">Students</h2>
+                        @if ($user->studentInfo->isEnrolledToCurrentAY())
+                            @if ($current_section)
+                                <div class="flex flex-col gap-y-4">
+                                    <h3 class="font-bold">Your Section: {{ $current_section->name }}</h3>
+                                    <div class="grid grid-cols-1 md:grid-cols-2 gap-3 font-fs">
+                                        <x-card class="py-3 px-4 flex flex-row gap-x-4">
+                                            <div>
+                                                <div
+                                                    class="w-12 aspect-square border border-accent rounded-full inline-flex justify-center items-center text-xl text-accent">
+                                                    <i class="fa-solid fa-user-graduate"></i>
+                                                </div>
+                                            </div>
+                                            <div class="flex flex-col">
+                                                <h2 class="font-semibold uppercase text-sm text-gray-500">Students</h2>
 
-                                        <div>
-                                            <span class="text-2xl truncate font-semibold">0</span>
-                                        </div>
-                                    </div>
-                                </x-card>
+                                                <div>
+                                                    <span
+                                                        class="text-2xl truncate font-semibold">{{ $current_section->students->count() }}</span>
+                                                </div>
+                                            </div>
+                                        </x-card>
 
-                                <x-card class="py-3 px-4 flex flex-row gap-x-4">
-                                    <div>
-                                        <div
-                                            class="w-12 aspect-square border border-red-700 rounded-full inline-flex justify-center items-center text-xl text-red-700">
-                                            <i class="fa-solid fa-book-open"></i>
-                                        </div>
-                                    </div>
-                                    <div class="flex flex-col">
-                                        <h2 class="font-semibold uppercase text-sm text-gray-500">Subjects</h2>
+                                        <x-card class="py-3 px-4 flex flex-row gap-x-4">
+                                            <div>
+                                                <div
+                                                    class="w-12 aspect-square border border-red-700 rounded-full inline-flex justify-center items-center text-xl text-red-700">
+                                                    <i class="fa-solid fa-book-open"></i>
+                                                </div>
+                                            </div>
+                                            <div class="flex flex-col">
+                                                <h2 class="font-semibold uppercase text-sm text-gray-500">Subjects</h2>
 
-                                        <div>
-                                            <span class="text-2xl truncate font-semibold">0</span>
-                                        </div>
+                                                <div>
+                                                    <span
+                                                        class="text-2xl truncate font-semibold">{{ $current_section->subjects->count() }}</span>
+                                                </div>
+                                            </div>
+                                        </x-card>
                                     </div>
-                                </x-card>
-                            </div>
-                        </div>
+                                </div>
 
-                        <x-table.wrapper>
-                            <x-table.head>
-                                <tr>
-                                    <x-table.header-cell :scope="__('col')">Subject Name</x-table.header-cell>
-                                    <x-table.header-cell :scope="__('col')">Faculty</x-table.header-cell>
-                                </tr>
-                            </x-table.head>
-                            <x-table.body>
-                                {{-- @forelse ($subjects as $sub) --}}
-                                @for ($i = 0; $i < 8; $i++)
-                                    <tr class="bg-white border-b">
-                                        <x-table.header-cell :scope="__('row')">Reading and Writing</x-table.header-cell>
-                                        <x-table.regular-cell>Rondina, David</x-table.regular-cell>
-                                    </tr>
-                                @endfor
-                                {{-- @endforelse --}}
-                            </x-table.body>
-                        </x-table.wrapper>
+                                <x-table.wrapper>
+                                    <x-table.head>
+                                        <tr>
+                                            <x-table.header-cell :scope="__('col')">Subject Name</x-table.header-cell>
+                                            <x-table.header-cell :scope="__('col')">Faculty</x-table.header-cell>
+                                        </tr>
+                                    </x-table.head>
+                                    <x-table.body>
+                                        @forelse ($current_section->subjects as $sub)
+                                            {{-- @for ($i = 0; $i < 8; $i++) --}}
+                                            <tr class="bg-white border-b">
+                                                <x-table.header-cell
+                                                    :scope="__('row')">{{ $sub->subject->name }}</x-table.header-cell>
+                                                <x-table.regular-cell>{{ $sub->faculty->user->profile->getFullName() }}</x-table.regular-cell>
+                                            </tr>
+                                        @empty
+                                            <x-table.regular-cell colspan="2">No subjects
+                                                found.</x-table.regular-cell>
+                                            {{-- @endfor --}}
+                                        @endforelse
+                                    </x-table.body>
+                                </x-table.wrapper>
+                            @else
+                                <p class="text-center text-gray-500">You are not in a section yet.</p>
+                            @endif
+                        @else
+                            <p class="text-center text-gray-500">You are not currently enrolled.</p>
+                        @endif
                     </x-card>
                 </div>
             </div>
@@ -113,28 +132,28 @@
                         </div>
 
                         <ul class="max-h-[300px] flex flex-col gap-y-2 text-sm overflow-y-auto">
-                            {{-- @forelse ($weekly_apps as $app) --}}
-                            {{-- @php
+                            @forelse ($upcoming_apps as $app)
+                                @php
                                     $respondent = $app->complaint->respondent;
-                                @endphp --}}
+                                @endphp
 
-                            @for ($i = 0; $i < 6; $i++)
                                 <li class="flex justify-between items-center">
                                     <div class="flex flex-col gap-y-1">
-                                        <span class="font-semibold">Monday, April 29, 2023</span>
-                                        <span class="text-xs text-gray-500">5:00 PM - 6:00
-                                            PM{{-- {{ Carbon::parse($app->start_date)->format('l, M. d') . ', ' . Carbon::parse($app->start_date)->format('g:i A') . ' - ' . Carbon::parse($app->end_date)->format('g:i A') }} --}}</span>
+                                        <span
+                                            class="font-semibold">{{ Carbon::parse($app->start_date)->format('l, M d, Y') }}</span>
+                                        <span
+                                            class="text-xs text-gray-500">{{ Carbon::parse($app->start_date)->format('g:i A') . ' - ' . Carbon::parse($app->end_date)->format('g:i A') }}</span>
                                     </div>
 
                                     <div class="tooltip" data-tip="View">
-                                        <a href="#" class="btn btn-outline btn-primary btn-circle"><i
+                                        <a href="{{ route('student.appointments.show', $app->id) }}"
+                                            class="btn btn-outline btn-primary btn-circle"><i
                                                 class="ri-arrow-right-up-line"> </i></a>
                                     </div>
                                 </li>
-                            @endfor
-                            {{-- @empty --}}
-                            <li class="text-center text-gray-500">There are no upcoming appointments.</li>
-                            {{-- @endforelse --}}
+                            @empty
+                                <li class="text-center text-gray-500">There are no upcoming appoitments.</li>
+                            @endforelse
                         </ul>
                     </div>
                 </x-card>
